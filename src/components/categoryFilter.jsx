da9fdeaaa5ffc6a8836b2767227c43d6a2c29681
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2023-02-15 15:10:28 +0530

    renndering category list

--- src/components/categoryFilter.jsx
+++ src/components/categoryFilter.jsx
@@ -1,5 +1,31 @@
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 const CategoryFilter = (props) => {
-  return <aside>category filter</aside>;
+  return (
+    <aside>
+      <ul>
+        {CATEGORIES.map((category) => (
+          <li key={category.color} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: category.color }}
+            >
+              {category.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 };
 
 export default CategoryFilter;
